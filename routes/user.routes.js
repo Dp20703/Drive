dba220da0f1ff0e9a79f8ba7900cf34ec0fe201e
dp <codewithdp2073@gmail.com>
2024-12-08 14:54:47 +0530

    user created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
 
 
 
@@ -14,21 +15,34 @@ router.get('/register', (req, res) => {
     res.render("register")
 })
 router.post('/register',
+    //validation:
     body('email').trim().isEmail().isLength({ min: 13 }),
     body('username').trim().isLength({ min: 3 }),
     body('password').trim().isLength({ min: 5 }),
-    (req, res) => {
-        const errors = validationResult(req);
+    async (req, res) => {
+        const errors = validationResult(req);//store collection of errors:
+        //if errors is not empty:
         if (!errors.isEmpty()) {
-           return res.status(400).json({
-            errors:errors.array(),
-            message:'invalid data'
-           })
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'invalid data'
+            })
         }
-        else {
-            console.log(req.body)
-            res.send("User Regsitered Successfully")
-        }
+        //if there are no errors:
+        const { username, email, password } = req.body;
+        const newUser = await userModel.create({
+            username: username,
+            email: email,
+            password: password
+        })
+        res.json(newUser);
+        console.log("new user created\n", newUser)
+
+
+        // else {
+        //     console.log(req.body)
+        //     res.send("User Regsitered Successfully")
+        // }
 
     })
 
