2246802ab7879e7d6c2e2ac1d05dd758b2b3286c
dp <codewithdp2073@gmail.com>
2024-12-08 12:04:56 +0530

    user registration

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 const userRouter = require('./routes/user.routes');//Required the userRoutes:
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+//built-in middlewares:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //user Routes:
 app.use('/user', userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 /* Prefix with /user */
 router.get('/test', (req, res) => {
     res.send("user tested")
 })
 
+//registration routes:
+router.get('/register', (req, res) => {
+    res.render("register")
+})
+router.post('/register', (req, res) => {
+    console.log(req.body)
+    res.send("User Regsitered Successfully")
+})
 
 module.exports = router;
