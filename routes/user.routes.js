520ca8f7a5f695aec7a82dbec47f94035a274a11
dp <codewithdp2073@gmail.com>
2024-12-08 13:08:00 +0530

    registration validation added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 
@@ -12,9 +13,23 @@ router.get('/test', (req, res) => {
 router.get('/register', (req, res) => {
     res.render("register")
 })
-router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send("User Regsitered Successfully")
-})
+router.post('/register',
+    body('email').trim().isEmail().isLength({ min: 13 }),
+    body('username').trim().isLength({ min: 3 }),
+    body('password').trim().isLength({ min: 5 }),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+           return res.status(400).json({
+            errors:errors.array(),
+            message:'invalid data'
+           })
+        }
+        else {
+            console.log(req.body)
+            res.send("User Regsitered Successfully")
+        }
+
+    })
 
 module.exports = router;
